66de57722dc6d12119c677abde04c1250ed6e345
kazmy <nehaluddin.dpe@gmail.com>
2023-05-11 10:20:58 +0530

    assignmnet

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,32 +4,10 @@ const controler = require('../controllers/controller')
 const middleware = require('../middleware/middleware')
 
 
-// const authonticate = function(req,res,next){
-//     let token = req.headers["x-auth-token"];
-//     if (!token) {return res.send({ status: false, msg: "token must be present" });}
-
-//     let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
-//     if (!decodedToken)
-//       return res.send({ status: false, msg: "token is invalid" });
-    
-//     let id = req.params.userid
-//     if(id == decodedToken.userId){
-//       console.log({status:"token is valid and user match"})
-//       next()
-//     }
-//     else{
-//       res.send({status: "Not valid token for particular user",msg :decodedToken})
-//     }
-// }
-
-
-
 router.post('/createUser',controler.createUser)
 router.post('/loginUser', controler.loginUser)
 router.get('/user/:userid', middleware.authorisation, controler.user)
 router.put('/updateUser/:userid', middleware.authorisation, controler.updateUser)
 router.put('/deleteUser/:userid', middleware.authorisation, controler.deleteUser)
-
-
 
 module.exports = router;
